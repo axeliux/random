cfb4c4bad7a5cfd15892012066527f36dd35f599
Axel Velazquez <axel.david.velazquez@gmail.com>
2014-02-17 18:01:51 -0600

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,3 @@
-
 public class Test {
 
 		public static void main(String str[]){
@@ -52,4 +51,32 @@ public class Test {
 				}
 			}
 		}
+
+/**
+ * sum permutation
+ * /
+public static void all(int t){
+	String R[][] = new String[t+1][];
+	R[0] =new String[0];
+	for(int i = 1; i <= t; i++){
+		R[i] = new String[(i/2) + 1];
+		for( int j = 1; j <= i/2; j++){
+			R[i][j] = (i-j) + "+" + j + "";
+		}
+	}
+	System.out.println(" -> ["+t+"] =  ");
+	for(int i = t; i >= 0 ; i-- ){
+		for(int j = 1; j < R[i].length; j++){
+			System.out.print(R[i][j]);
+			for(int x= i; x < t; x++){
+				 System.out.print("+1");
+			}
+			System.out.print(", ");
+			
+		}
+		System.out.println();
+	}
+	System.out.println();
+}
+
 }
