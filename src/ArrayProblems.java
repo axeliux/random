6d844026cdefddcc13ba62f54add8ef75da21353
Axel Velazquez <axel.david.velazquez@gmail.com>
2014-02-23 21:19:45 -0600

    Update ArrayProblems.java

--- src/ArrayProblems.java
+++ src/ArrayProblems.java
@@ -40,4 +40,36 @@ public class ArrayProblems{
 			System.out.println("cannot find Sum K = "+ k);
 		}
 	}
+	
+	public static void ZeroSum(int A[]){
+		int currentKey = 0;
+		Hashtable<Integer,List<Integer>> table = new Hashtable<Integer,List<Integer>>();
+		
+		for(int i = 0; i < A.length; i++){
+			currentKey += A[i];
+			if(table.containsKey(currentKey)){
+				table.get(currentKey).add(i);
+			}else{
+				List<Integer> list = new ArrayList<Integer>();
+				list.add(i);
+				table.put(currentKey,list);
+			}
+		}
+		
+		Set<Integer> keys = table.keySet();
+		for(Integer key: keys){
+			List<Integer> list = table.get(key);
+			if(list.size() > 1){
+				for(int i = 0; i < list.size() -1; i++){
+					for(int j= i+1; j < list.size(); j++){
+						for(int k = list.get(i) + 1; k <= list.get(j); k++){
+							System.out.print(A[k] + " + ");
+						}
+						System.out.println(" = 0");
+					}
+				}
+			}
+		}
+
+	}
 }
